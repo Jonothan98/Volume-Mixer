105c1028535deedd7e34d5a03067d45dabac6996
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2023-08-01 13:18:33 -0500

    app: created .net application and added audio api core package. Also assigned the default input and output devices

---
+++ source/Volume Mixer/VolumeMixer.cs
@@ -0,0 +1,41 @@
+﻿using AudioSwitcher.AudioApi.CoreAudio;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volume_Mixer
+{
+    internal class VolumeMixer
+    {
+        CoreAudioDevice defaultPlaybackDevice;
+        CoreAudioDevice defaultInputDevice;
+
+        
+
+        static void Main(string[] args)
+        {
+            SerialPort port = new SerialPort();
+            VolumeMixer volumeMixer = new VolumeMixer();
+
+            port.BaudRate = 9600;
+            port.PortName = "COM3";
+
+
+            volumeMixer.setCoreAudioDevices();
+
+
+        }
+
+        void setCoreAudioDevices()
+        {
+            defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+            defaultInputDevice = new CoreAudioController().DefaultCaptureDevice;
+            Console.WriteLine($"{defaultPlaybackDevice.FullName} + {defaultInputDevice.FullName}");
+        }
+
+
+    }
+}
