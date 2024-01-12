694ca5d60b6f38ff876c3b4c1c3be36e26276c89
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2024-01-12 10:56:58 -0600

    app: cleaned up arduino code by removing old commented code, and added code for mute mic and mute all buttons

--- source/source.ino
+++ source/source.ino
@@ -1,14 +1,38 @@
+#include <ezButton.h>
+
 const int NUMBER_OF_KNOBS = 5;
 const int analogInputs[NUMBER_OF_KNOBS] = {A0,A1,A2,A3,A4};
+const int digitalPins[4] = {2,3,4,5};
 
+const int muteKeyPin = 2; 
+const int muteLedPin = 3;
+const int pauseKeyPin = 4;
+const int pauseLedPin = 5;
+
+ezButton muteButton(muteKeyPin);
+ezButton pauseButton(pauseKeyPin);
+
+int muteKeyState = 0;
+int pauseKeyState = 0;
+int micVolumeLevel = 1023;
+bool muteAll = false;
 int analogKnobValues[NUMBER_OF_KNOBS];
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   for(int i = 0; i < NUMBER_OF_KNOBS; i++){
     pinMode(analogInputs[i],INPUT);
   }
-  Serial.begin(9600);
+
+  pinMode(muteLedPin,OUTPUT);
+  pinMode(muteKeyPin,INPUT_PULLUP);
+
+  pinMode(pauseLedPin,OUTPUT);
+  pinMode(pauseKeyPin, INPUT_PULLUP);
+
+  muteButton.setDebounceTime(100);
+   
 }
 
 void loop() {
@@ -16,11 +40,42 @@ void loop() {
     updateSliderValues();
     sendSliderValues();
     delay(10);
-    //printSliderValues();
+
+    muteKeyState = digitalRead(muteKeyPin);
+    pauseKeyState = digitalRead(pauseKeyPin);
+    muteButton.loop();
+    pauseButton.loop();
+
+    if(muteButton.isPressed()){
+      toggleLed(muteLedPin);
+    }
+
+    if(pauseButton.isPressed()){
+      toggleLed(pauseLedPin);
+    }
+
+}
+
+void toggleLed(int ledPin){
+  if(digitalRead(ledPin) == LOW){
+    digitalWrite(ledPin,HIGH);
+    if(ledPin == 3){
+      micVolumeLevel = 0;
+    } else {
+      muteAll = true;
+    }
+  } else {
+    digitalWrite(ledPin,LOW);
+    if(ledPin == 3){
+      micVolumeLevel = 1023;
+    } else {
+      muteAll = false;
+    }
+  }
 }
 
 void updateSliderValues(){
-  for(int i = 0; i < NUMBER_OF_KNOBS; i++){
+  for(int i = 0; i < NUMBER_OF_KNOBS-1; i++){
     analogKnobValues[i] = analogRead(analogInputs[i]);
   }
 }
@@ -29,7 +84,13 @@ void sendSliderValues(){
   String builtString = String("");
 
   for(int i = 0; i < NUMBER_OF_KNOBS; i++){
-    builtString += String((int)map(analogKnobValues[i], 0, 1023, 0, 100));
+    if(i == 4){
+      builtString += String(micVolumeLevel);
+    } else if(muteAll == true) {
+      builtString += String((int) 0);
+    } else {
+      builtString += String((int)analogKnobValues[i]);
+    }
 
     if(i < NUMBER_OF_KNOBS -1){
       builtString += String("|");
@@ -37,6 +98,7 @@ void sendSliderValues(){
   }
 
   Serial.println(builtString);
+
 }
 
 void printSliderValues(){
